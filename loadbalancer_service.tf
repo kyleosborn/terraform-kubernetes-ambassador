ab7b8ec0f553327f1652c06b40e1cc168b8da4be
Kyle Osborn <32040026+kyleosborn@users.noreply.github.com>
2021-03-20 21:53:04 +0000

    Update loadbalancer_service.tf

--- loadbalancer_service.tf
+++ loadbalancer_service.tf
@@ -20,7 +20,7 @@ resource "kubernetes_service" "this_loadbalancer" {
       content {
         name        = port.value.name
         port        = port.value.port
-        target_port = port.value.target_port
+        target_port = port.value.container_port
       }
     }
 
